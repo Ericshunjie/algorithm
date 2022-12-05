67da138d56638f01c833b7106e2922840ad5a2bb
yuanshunjie <shunjieyuan811@163.com>
2022-12-05 23:30:28 +0800

    1201night

--- 滑动窗口最大值.py
+++ 滑动窗口最大值.py
@@ -1,20 +1,22 @@
-import collections
-class Solution:
+class Solution(object):
     def maxSlidingWindow(self, nums, k):
-        if not nums or k == 0: return []
-        deque = collections.deque()
-        for i in range(k): # 未形成窗口
-            while deque and deque[-1] < nums[i]:
-                deque.pop()
-            deque.append(nums[i])
-        res = [deque[0]]
-        for i in range(k, len(nums)): # 形成窗口后
-            if deque[0] == nums[i - k]:
-                deque.popleft()
-            while deque and deque[-1] < nums[i]:
-                deque.pop()
-            deque.append(nums[i])
-            res.append(deque[0])
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        #遍历的方法复杂度为 nk 超时
+        # 利用大顶堆数据结构 可以优化成nlog(k)
+
+        # 先将k个数据放入大顶堆
+        n = len(nums)
+        q = [(-nums[i], i)for i in range(k)]
+        heapq.heapify(q)
+        res = [-q[0][0]]
+        for i in range(k, n):
+            heapq.heappush(q, (-nums[i], i))
+            # 当堆顶的index 小于等于 i-k的时候，这个元素需要pop
+            while q[0][1] <= i-k:
+                heapq.heappop(q)
+            res.append(-q[0][0])
         return res
-s = Solution()
-r = s.maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3)
