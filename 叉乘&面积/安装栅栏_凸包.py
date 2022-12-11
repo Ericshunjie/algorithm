c206839ce40da9463f384b10fcdf0ad2605d8ed7
yuanshunjie <shunjieyuan811@163.com>
2022-12-11 23:48:46 +0800

    1211night

--- 叉乘&面积/安装栅栏_凸包.py
+++ 叉乘&面积/安装栅栏_凸包.py
@@ -49,3 +49,50 @@ class Solution(object):
             if p == leftindex:
                 break
         return res
+
+# 解法二 Andrew
+class Solution(object):
+    def outerTrees(self, trees):
+        """
+        :type trees: List[List[int]]
+        :rtype: List[List[int]]
+        """
+
+        # andrew算法，将凸包问题转化成求上凸包和下凸包问题
+        # 将点按照x升序 y升序排序
+        # 从左到右 遍历一次，如果新的点在左侧那就是正常的 用叉乘判断新的点在左侧还是右侧
+        # 如果新的点在右侧，那么就要把栈顶的点pop出去 再判断，如果不行继续pop
+        # 在一个从右到左 找到上半凸包
+        def cross(p1, p2, p3):
+            v1 = [p2[0] - p1[0], p2[1] - p1[1]]
+            v2 = [p3[0] - p1[0], p3[1] - p1[1]]
+            return v1[0] * v2[1] - v1[1] * v2[0]
+
+        n = len(trees)
+        if n < 4: return trees
+        # 按照x升序 x相同情况y升序
+        trees.sort()
+        # print(trees)
+        stack = [0]
+        vis = [0] * n
+        # vis[0]先不能等于1 后面求上半的时候要用
+
+        # 寻找下凸包
+        for i in range(1, n):
+            while len(stack) > 1 and cross(trees[stack[-2]], trees[stack[-1]], trees[i]) < 0:
+                # print(i)
+                jj = stack.pop()
+                vis[jj] = 0
+            stack.append(i)
+            vis[i] = 1
+            # print(i, stack)
+        m = len(stack)
+        for i in range(n - 2, -1, -1):
+            if not vis[i]:
+                while len(stack) > m and cross(trees[stack[-2]], trees[stack[-1]], trees[i]) < 0:
+                    stack.pop()
+
+                stack.append(i)
+                # print(i,stack)
+        return [trees[i] for i in stack[:-1]]
+
