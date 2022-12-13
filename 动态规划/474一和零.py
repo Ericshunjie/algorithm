9bdae4f7319d9ae77f4b3e299d9c45dae4264d85
yuanshunjie <shunjieyuan811@163.com>
2022-12-13 23:55:38 +0800

    1213night

--- 动态规划/474一和零.py
+++ 动态规划/474一和零.py
@@ -1,32 +1,33 @@
-class Solution:
-    def findMaxForm(self, strs, m: int, n: int) -> int:
-        # 类似01背包问题
-        # dp[i][j] 为最多又i个0 j个1的最大子集数
-        # dp[i][j] = max(dp[i][j], dp[i-num0][j-num1] + 1)
-        # dp[0][0] = 0
-
-        nn = len(strs)
-        nums0 = [0] * nn
-        nums1 = [0] * nn
-        dp = [[0] * (n+1) for _ in range(m+1)]
-        print(len(dp),len(dp[0]))
-        for i in range(nn):
-            for s in strs[i]:
-                if s == '0':
-                    nums0[i] += 1
+class Solution(object):
+    def findMaxForm(self, strs, m, n):
+        """
+        :type strs: List[str]
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        def counts(s):
+            ones = 0
+            zeros = 0
+            for char in s:
+                if char == '0':
+                    zeros += 1
                 else:
-                    nums1[i] += 1
-        print(nums0,nums1)
-        for i in range(nn):
-            for j in range(m,-1,-1):
-                for k in range(n,-1,-1):
-                    print(i,j,k)
-                    if j-nums0[i] >= 0 and k-nums1[i] >= 0:
-                        print(j-nums0[i],k-nums1[i])
-                        print(dp[j][k])
-                        print(dp[j-nums0[i]][k-nums1[i]])
-                        dp[j][k] = max(dp[j][k], dp[j-nums0[i]][k-nums1[i]] + 1)
-        return dp[-1][-1]
-
-s = Solution()
-s.findMaxForm(["10","0001","111001","1","0"],5,3)
+                    ones += 1
+            return zeros, ones
+        # 传统的01背包问题有一个容量 但是这里有两个容量mn
+        # 所以这里是一个三维背包问题dp[i][j][k] 代表前i个物体子集中最多有j个0 k个1的最大长度
+        # 最终求dp[l][m][n]
+        # dp[i][j][k] = max(dp[i-1][j][k],
+        #                     dp[i-1][j-zeros][k-ones] + 1)
+        l = len(strs)
+        dp = [[[0] * (n+1) for _ in range(m+1)] for j in range(l+1)]
+        for i in range(1, l+1):
+            zeros,ones = counts(strs[i-1])
+            for j in range(m+1):
+                for k in range(n+1):
+                    #不能把物体i放进去
+                    dp[i][j][k] = dp[i-1][j][k]
+                    if j >= zeros and k >= ones:
+                        dp[i][j][k] = max(dp[i][j][k], dp[i-1][j-zeros][k-ones] + 1)
+        return dp[-1][-1][-1]
